22198d098ef05548b4c37675e7c5447551963061
polydome <polydome@outlook.com>
2019-09-19 17:41:53 +0200

    Improve enum converters

--- app/src/main/java/com/example/polydome/projectgutman/data/Converters.kt
+++ app/src/main/java/com/example/polydome/projectgutman/data/Converters.kt
@@ -2,46 +2,19 @@ package com.example.polydome.projectgutman.data
 
 import androidx.room.TypeConverter
 import com.example.polydome.projectgutman.domain.model.Difficulty
-import java.lang.Exception
-
-class UnimplementedTriggerTypeValueException(value: Byte): Exception(
-    "TriggerType of value [$value] unknown"
-)
-
-class UnimplementedTriggerTypeException(triggerType: TriggerType): Exception(
-    "TriggerType [$triggerType] unknown"
-)
-
-class UnimplementedDifficultyValueException(value: Byte): Exception(
-    "Difficulty of value [$value] unknown"
-)
 
 class Converters {
 
     @TypeConverter
-    fun toTriggerType(triggerTypeValue: Byte): TriggerType = when (triggerTypeValue) {
-        0.toByte() -> TriggerType.COUNT
-        else -> throw UnimplementedTriggerTypeValueException(triggerTypeValue)
-    }
+    fun toTriggerType(value: String): TriggerType = TriggerType.valueOf(value)
 
     @TypeConverter
-    fun fromTriggerType(triggerType: TriggerType): Byte = when (triggerType) {
-        TriggerType.COUNT -> 0
-    }
+    fun fromTriggerType(triggerType: TriggerType): String = triggerType.name
 
     @TypeConverter
-    fun toDifficulty(difficultyValue: Byte): Difficulty = when (difficultyValue) {
-        0.toByte() -> Difficulty.EASY
-        1.toByte() -> Difficulty.MEDIUM
-        2.toByte() -> Difficulty.HARD
-        else -> throw UnimplementedDifficultyValueException(difficultyValue)
-    }
+    fun toDifficulty(value: String): Difficulty = Difficulty.valueOf(value)
 
     @TypeConverter
-    fun fromDifficulty(difficulty: Difficulty): Byte = when (difficulty) {
-        Difficulty.EASY -> 0
-        Difficulty.MEDIUM -> 1
-        Difficulty.HARD -> 2
-    }
+    fun fromDifficulty(difficulty: Difficulty): String = difficulty.name
 
 }
