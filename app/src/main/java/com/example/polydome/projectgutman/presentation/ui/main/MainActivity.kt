00947a6ad7b1801ec82627d6180a39a56089c89b
polydome <polydome@outlook.com>
2019-09-19 20:23:01 +0200

    Setup recycler view in `MainActivity`

--- app/src/main/java/com/example/polydome/projectgutman/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/polydome/projectgutman/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.polydome.projectgutman.presentation.ui.main
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.polydome.projectgutman.R
 import com.example.polydome.projectgutman.presentation.ui.ActionsListAdapter
 import com.example.polydome.projectgutman.presentation.ui.common.BaseActivity
 import javax.inject.Inject
@@ -10,9 +12,9 @@ class MainActivity : BaseActivity() {
 
     @Inject lateinit var actionsListAdapter: ActionsListAdapter
 
-    lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView
 
-    val layoutResId: Int = 0
+    private val layoutResId: Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,14 @@ class MainActivity : BaseActivity() {
         presentationComponent.inject(this)
 
         setContentView(layoutResId)
+
+        recyclerView = findViewById(R.id.actions_rv)
+
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
 
         recyclerView.adapter = actionsListAdapter
     }
