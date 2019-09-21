ec7e678bd95361e3cc30a44cd3b88a7542a4de5a
polydome <polydome@outlook.com>
2019-09-21 19:37:06 +0200

    Modify GoalTriggerEntity to include action id

--- app/src/main/java/com/example/polydome/projectgutman/data/AppDatabase.kt
+++ app/src/main/java/com/example/polydome/projectgutman/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @TypeConverters(value = [Converters::class])
-@Database(entities = [ActionEntity::class, GoalTriggerEntity::class], version = 1)
+@Database(entities = [ActionEntity::class, GoalTriggerEntity::class], version = 2)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun actionEntityDao(): ActionEntityDao
     abstract fun goalTriggerDao(): GoalTriggerDao
--- app/src/main/java/com/example/polydome/projectgutman/data/GoalTriggerEntity.kt
+++ app/src/main/java/com/example/polydome/projectgutman/data/GoalTriggerEntity.kt
@@ -2,13 +2,22 @@ package com.example.polydome.projectgutman.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.example.polydome.projectgutman.domain.model.GoalTrigger
 
 @Entity(tableName = "goalTrigger")
 class GoalTriggerEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int?,
-    @ColumnInfo(name = "triggerType") val triggerType: TriggerType,
-    @ColumnInfo(name = "value") val value: Long
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ForeignKey(childColumns = ["actionId"], parentColumns = ["id"], entity = ActionEntity::class)
+    @ColumnInfo(name = "actionId")
+    val actionId: Int?,
+
+    @ColumnInfo(name = "triggerType")
+    val triggerType: TriggerType,
+
+    @ColumnInfo(name = "value")
+    val value: Long
 )
 
