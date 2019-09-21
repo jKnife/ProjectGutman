37695c6f16a3db757ac5551b4e057e78c9e60bc8
polydome <polydome@outlook.com>
2019-09-21 19:43:21 +0200

    Add `getByActionId` to GoalTriggerRepository

--- app/src/main/java/com/example/polydome/projectgutman/repository/GoalTriggerRepository.kt
+++ app/src/main/java/com/example/polydome/projectgutman/repository/GoalTriggerRepository.kt
@@ -1,12 +1,26 @@
 package com.example.polydome.projectgutman.repository
 
+import com.example.polydome.projectgutman.data.GoalTriggerDao
+import com.example.polydome.projectgutman.data.TriggerType
 import com.example.polydome.projectgutman.domain.model.GoalTrigger
 import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
 
-class GoalTriggerRepository {
+class GoalTriggerRepository
+@Inject constructor(private val goalTriggerDao: GoalTriggerDao) {
 
     fun insert(goalTrigger: GoalTrigger): Completable = Completable.create {
 
     }
+
+    fun getByActionId(actionId: Int): Observable<List<GoalTrigger>> =
+        goalTriggerDao.getByActionId(actionId)
+            .map { it.map { entity ->
+                    when (entity.triggerType) {
+                        TriggerType.COUNT -> GoalTrigger.Count(entity.value.toInt())
+                    }
+                }
+            }
 
 }
