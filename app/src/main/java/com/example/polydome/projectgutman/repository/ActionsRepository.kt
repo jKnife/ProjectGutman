d80d626447f041f455ae1f4f62320075897f0ea4
polydome <polydome@outlook.com>
2019-09-23 22:52:03 +0200

    Implement `ActionsRepository#insertAction`

--- app/src/main/java/com/example/polydome/projectgutman/repository/ActionsRepository.kt
+++ app/src/main/java/com/example/polydome/projectgutman/repository/ActionsRepository.kt
@@ -1,17 +1,37 @@
 package com.example.polydome.projectgutman.repository
 
+import com.example.polydome.projectgutman.data.ActionEntity
+import com.example.polydome.projectgutman.data.ActionEntityDao
+import com.example.polydome.projectgutman.data.TriggerType
 import com.example.polydome.projectgutman.domain.model.Action
+import com.example.polydome.projectgutman.domain.model.Goal
 import io.reactivex.Completable
 import io.reactivex.Maybe
+import javax.inject.Inject
 
-class ActionsRepository {
+class ActionsRepository
+@Inject constructor(private val actionEntityDao: ActionEntityDao) {
 
-    fun findAction(actionId: Int): Maybe<Action> = Maybe.create {
+    fun insertAction(name: String, goal: Goal): Completable = Completable.defer {
+        val triggerType = when (goal) {
+            is Goal.Count -> TriggerType.COUNT
+        }
 
+        val targetValue: Long = when (goal) {
+            is Goal.Count -> goal.targetCount.toLong()
+        }
+
+        val actionEntity = ActionEntity(
+            id = 0,
+            name = name,
+            triggerType = triggerType,
+            currentValue = 0,
+            targetValue = targetValue
+        )
+
+        actionEntityDao.insertAction(actionEntity)
     }
 
-    fun updateAction(action: Action): Completable = Completable.create {
-
-    }
+    fun findAction(actionId: Int): Maybe<Action> = Maybe.empty()
 
 }
