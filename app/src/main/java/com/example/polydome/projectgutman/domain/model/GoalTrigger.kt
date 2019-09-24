9793fb796ed552c4dfaf5bd250590807ea7a6634
polydome <polydome@outlook.com>
2019-09-24 18:57:59 +0200

    Temporarily remove Duration from GoalTrigger class

--- app/src/main/java/com/example/polydome/projectgutman/domain/model/GoalTrigger.kt
+++ app/src/main/java/com/example/polydome/projectgutman/domain/model/GoalTrigger.kt
@@ -1,7 +1,6 @@
 package com.example.polydome.projectgutman.domain.model
 
 sealed class GoalTrigger {
-    data class Duration(val duration: Long): GoalTrigger()
+//    data class Duration(val duration: Long): GoalTrigger()
     data class Count(val count: Int): GoalTrigger()
-
 }
