d07e5ba97abb5b5c725491c4d7ac53f4cf0dd938
polydome <polydome@outlook.com>
2019-09-24 19:00:50 +0200

    Implement `GoalTriggerRepository#insert`

--- app/src/main/java/com/example/polydome/projectgutman/repository/GoalTriggerRepository.kt
+++ app/src/main/java/com/example/polydome/projectgutman/repository/GoalTriggerRepository.kt
@@ -1,6 +1,7 @@
 package com.example.polydome.projectgutman.repository
 
 import com.example.polydome.projectgutman.data.GoalTriggerDao
+import com.example.polydome.projectgutman.data.GoalTriggerEntity
 import com.example.polydome.projectgutman.data.TriggerType
 import com.example.polydome.projectgutman.domain.model.GoalTrigger
 import io.reactivex.Completable
@@ -10,8 +11,23 @@ import javax.inject.Inject
 class GoalTriggerRepository
 @Inject constructor(private val goalTriggerDao: GoalTriggerDao) {
 
-    fun insert(goalTrigger: GoalTrigger): Completable = Completable.create {
+    fun insert(actionId: Int, goalTrigger: GoalTrigger): Completable = Completable.create {
+        val value: Long = when (goalTrigger) {
+            is GoalTrigger.Count -> goalTrigger.count.toLong()
+        }
 
+        val triggerType = when (goalTrigger) {
+            is GoalTrigger.Count -> TriggerType.COUNT
+        }
+
+        val entity = GoalTriggerEntity(
+            id = null,
+            value = value,
+            triggerType = triggerType,
+            actionId = actionId
+        )
+
+        goalTriggerDao.insertTrigger(entity)
     }
 
     fun getByActionId(actionId: Int): Observable<List<GoalTrigger>> =
